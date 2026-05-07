2f8a09cd56638162ae7bf49df7e191d78481e7ba
Star <141683037+Star-Nord@users.noreply.github.com>
2026-05-07 23:11:58 +0400

    Delete summative 2 directory

--- summative 2/home.js
+++
@@ -1,4 +0,0 @@
-//this originated here but has been since been copied to all js files, it adds functionality to the dropdown menu
-document.querySelector('.dropdown').addEventListener('click', function() {
-    document.querySelector('.links').classList.toggle('open');
-});
